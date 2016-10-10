7b97ad6239e9cc17260ed41153aed461b15c0dfe
Jakob Fuhrmann <3326760+Jafu@users.noreply.github.com>
2016-10-10 13:28:44 +0200

    auto suggestions are now part of zsh config files

--- run.sh
+++ run.sh
@@ -4,8 +4,19 @@ vimrcPath=$(realpath dotVimrc)
 zshrcPath=$(realpath dotZshrc)
 gitconfigPath=$(realpath dotGitConfig)
 tmuxConfPath=$(realpath dotTmux.conf)
+fizshFolder=$(realpath .fizsh)
+autosuggestionsFolder=$(realpath .zsh-autosuggestions)
 
+#vim:
 rm ~/.vimrc; ln -s $vimrcPath ~/.vimrc
+
+# zsh:
+rm ~/.fizsh; ln -s $zshrcPath ~/.fizsh
+rm ~/.zsh-autosuggestions; ln -s $zshrcPath ~/.zsh-autosuggestions
 rm ~/.zshrc; ln -s $zshrcPath ~/.zshrc
+
+#git:
 rm ~/.gitconfig; ln -s $gitconfigPath ~/.gitconfig
+
+#tmux:
 rm ~/.tmux.conf; ln -s $tmuxConfPath ~/.tmux.conf
