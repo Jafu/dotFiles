914ef45efba3070decabb3c9417b3094e5397052
Jakob Fuhrmann <developer@UMT-10.fritz.box>
2016-12-23 16:25:37 +0100

    Replace zsh-autocomplete hack with mature solution

--- run.sh
+++ run.sh
@@ -4,15 +4,11 @@ vimrcPath=$(realpath dotVimrc)
 zshrcPath=$(realpath dotZshrc)
 gitconfigPath=$(realpath dotGitConfig)
 tmuxConfPath=$(realpath dotTmux.conf)
-fizshFolder=$(realpath .fizsh)
-autosuggestionsFolder=$(realpath .zsh-autosuggestions)
 
 #vim:
 rm ~/.vimrc; ln -s $vimrcPath ~/.vimrc
 
 # zsh:
-rm ~/.fizsh; ln -s $zshrcPath ~/.fizsh
-rm ~/.zsh-autosuggestions; ln -s $zshrcPath ~/.zsh-autosuggestions
 rm ~/.zshrc; ln -s $zshrcPath ~/.zshrc
 
 #git:
